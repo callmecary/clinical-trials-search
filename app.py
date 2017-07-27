3377bef6725b419090914306d60a848475667ab9
BadAssSocialTime <Cary>
2017-07-26 23:54:42 -0400

    test2

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-def wsgi_app(environ, start_response):
-    status = '200 OK'
-    response_headers = [('Content-type', 'text/plain')]
-    start_response(status, response_headers)
-    response_body = 'Hello World'
-    yield response_body.encode()
+from flask import Flask
+from flask_restplus import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+@api.route('/hello')
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
 
 if __name__ == '__main__':
-    from wsgiref.simple_server import make_server
-
-    httpd = make_server('localhost', 5555, wsgi_app)
-    httpd.serve_forever()
+    app.run(debug=True)
