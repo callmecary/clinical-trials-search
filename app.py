49a030e7bd3a59f82f051ee67e0d782b3c6b9ff1
BadAssSocialTime <Cary>
2017-07-27 00:23:31 -0400

    test5

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from flask import Flask
-from flask_restplus import Resource, Api
 
 app = Flask(__name__)
-api = Api(app)
 
-@api.route('/hello')
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+@app.route('/')
+def index():
+    return "Hello, World!"
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
