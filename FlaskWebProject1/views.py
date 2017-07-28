928bd81fc060d60494de0d5d4cc43544387d0914
Ma, Lianze <lma@hartreepartners.com>
2017-07-28 17:14:31 -0400

    added post

--- FlaskWebProject1/views.py
+++ FlaskWebProject1/views.py
@@ -5,11 +5,17 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template
 from FlaskWebProject1 import app, api, Resource
+import requests
+import json
 
 @api.route('/')
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
+    def post(self):
+        r=requests.post('https://clinicaltrialsapi.cancer.gov/v1/clinical-trials',data={"sites.org_state_or_province": ["NY"],"record_verification_date_gte": "2017-06-01","include": ["nci_id"]})
+        json_data = json.loads(r.text)
+        return json_data
 
 @app.route('/home')
 def home():
