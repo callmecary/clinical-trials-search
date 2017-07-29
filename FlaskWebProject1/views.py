634d648e96ce66a1d57c13923e34ad54f3a8d5a5
BadAssSocialTime <Cary>
2017-07-28 20:15:35 -0400

    clean code from template

--- FlaskWebProject1/views.py
+++ FlaskWebProject1/views.py
@@ -91,32 +91,3 @@ class Todo(Resource):
     def put(self, id):
         '''Update a task given its identifier'''
         return DAO.update(id, api.payload)
-
-@app.route('/home')
-def home():
-    """Renders the home page."""
-    return render_template(
-        'index.html',
-        title='Home Page',
-        year=datetime.now().year,
-    )
-
-@app.route('/contact')
-def contact():
-    """Renders the contact page."""
-    return render_template(
-        'contact.html',
-        title='Contact',
-        year=datetime.now().year,
-        message='Your contact page.'
-    )
-
-@app.route('/about')
-def about():
-    """Renders the about page."""
-    return render_template(
-        'about.html',
-        title='About',
-        year=datetime.now().year,
-        message='Your application description page.'
-    )
