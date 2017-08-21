dabc7779a6d5ae5775c7b474a15c106444477512
BadAssSocialTime <Cary>
2017-08-20 22:25:03 -0400

    add basic pagination and postgresql full text search

--- ClinicalTrials/views.py
+++ ClinicalTrials/views.py
@@ -8,12 +8,12 @@ from flask_restplus import Api, Resource, fields
 from werkzeug.contrib.fixers import ProxyFix
 from ClinicalTrials import app, api, Resource
 import json, requests
-#import db
+import db
 
 @api.route('/<string:keywords>&<int:page_num>&<int:page_size>')
 class Search(Resource):
     def get(self,keywords,page_num,page_size):
-        #data = db.search(keywords,page_num,page_size)
-        #return data
-        return 'comming soon.'
+        data = db.search('Gastric&Cancer',page_num,page_size)
+        print('Returning data...')
+        return data
 
