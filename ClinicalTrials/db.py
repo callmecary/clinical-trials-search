435958f0b14f0f8eaab77a041b8de23374156fad
BadAssSocialTime <Cary>
2017-09-07 21:40:01 -0400

    Search API: plus return description, official title, eligibility

--- ClinicalTrials/db.py
+++ ClinicalTrials/db.py
@@ -14,10 +14,13 @@ def search(keywords,which_page,size_per_page):
         query="""
 SELECT row_to_json(t)
 FROM(
-SELECT t_search.nct_id,t_search.start_date,t_search.brief_title,t_search.overall_status,t_search.phase
+SELECT t_search.nct_id,t_search.start_date,t_search.brief_title,t_search.official_title,t_search.description,t_search.eligibility,t_search.overall_status,t_search.phase
 FROM (SELECT s.nct_id as nct_id,
       	s.start_date as start_date,
+        s.official_title as official_title,
       	s.brief_title as brief_title,
+        d.description as description,
+        e.criteria as eligibility,
         s.overall_status as overall_status,
         s.phase as phase,
  		to_tsvector(s.brief_title) ||
@@ -25,11 +28,13 @@ FROM (SELECT s.nct_id as nct_id,
         to_tsvector(k.name) as 
 document
 FROM studies s
-INNER JOIN keywords k ON s.nct_id=k.nct_id) t_search
+INNER JOIN keywords k ON s.nct_id=k.nct_id
+INNER JOIN brief_summaries d ON s.nct_id=d.nct_id
+INNER JOIN eligibilities e ON e.nct_id=s.nct_id) t_search
 WHERE t_search.document @@ to_tsquery(\'{}\')
 AND t_search.phase <> 'N/A'
 AND t_search.overall_status = 'Recruiting'
-GROUP BY t_search.nct_id,t_search.start_date,t_search.brief_title,t_search.overall_status,t_search.phase
+GROUP BY t_search.nct_id,t_search.start_date,t_search.brief_title,t_search.official_title,t_search.description,t_search.eligibility,t_search.overall_status,t_search.phase
 ORDER BY t_search.start_date DESC
 OFFSET {}
 LIMIT {}) t""".format(keywords,(which_page-1)*size_per_page,size_per_page)
